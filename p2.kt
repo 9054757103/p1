9589f201a04f0d2886c94466e52b72f6dcabdb3a
Student <niralip1810@gmail.com>
2022-08-22 10:27:54 +0530

    Initial commit

---
+++ p2.kt
@@ -0,0 +1,13 @@
+fun main(){
+    val a:Int = 10
+    val b:Double = a.toDouble()
+    val c:String = "20"
+    val d:Int = c.toInt()
+    val e:Double = c.toDouble()
+
+    println("Interger Value:-"+a)
+    println("Double Value(from Interger):-"+b)
+    println("String Value:-"+c)
+    println("Interger Value (from String):-"+d)
+    println("Doule Value (from String):-"+e)
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    val a: Int = 20
+    val b: Double = 20.5
+    val c: String = "hello"
+    val d: Char = '$'
+    val e: Boolean = true
+    val f: Float = 1.5f
+
+    println("Float Value:-"+f)
+    println("Boolean Value:-"+e)
+    println("Character Value:-"+d)
+    println("String Value:-"+c)
+    println("Double Value:-"+b)
+    println("Interger Value:-"+a)
+}
